1ce694d2a5d993a1ffe7800d9b4052927fa815da
ryan <ryan@RYAN-PC.home>
2017-09-02 11:53:19 -0400

    5

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -19,41 +19,44 @@ using namespace std;
 bool isPrime(int x);
 int sumBtw(int x, int y);
 void leapYr400(int yr);
+void numTriangle(int x);
 
 // Problem 1, and test calls for future problems
 int main(){
 	cout << "Problem 1" << endl;
 	cout << "Hello world" << endl;
 
-	cout << "*****" << endl;
+	cout << "*************************" << endl;
 	cout << "Problem 2" << endl;
 	cout << isPrime(2) << endl; // Expected: true (1)
 	cout << isPrime(1) << endl; // Expected: false (0)
 	cout << isPrime(29) << endl; // Expected: true (1)
 
-	cout << "*****" << endl;
+	cout << "*************************" << endl;
 	cout << "Problem 3" << endl;
 	cout << sumBtw(1,10) << endl; // Expected: 44
 	cout << sumBtw(10,1) << endl; // Expected: 44
 	cout << sumBtw(2,2) << endl; // Expected: 0
 
-	cout << "*****" << endl;
+	cout << "*************************" << endl;
 	cout << "Problem 4" << endl;
 	leapYr400(2017); // Expected: 2020 to 2416 in increments of 4, excluding 2100, 2200, 2300
 
-	cout << "*****" << endl;
+	cout << "*************************" << endl;
 	cout << "Problem 5" << endl;
+	numTriangle(4); // Expected: refer to lab instructions, problem 5
+	numTriangle(5); // Expected: refer to lab instructions, problem 5
 
-	cout << "*****" << endl;
+	cout << "*************************" << endl;
 	cout << "Problem 6" << endl;
 
-	cout << "*****" << endl;
+	cout << "*************************" << endl;
 	cout << "Problem 7" << endl;
 
-	cout << "*****" << endl;
+	cout << "*************************" << endl;
 	cout << "Problem 8" << endl;
 
-	cout << "*****" << endl;
+	cout << "*************************" << endl;
 	cout << "Problem 9" << endl;
 	return 0;
 }
@@ -118,5 +121,42 @@ void leapYr400(int yr){
 }
 
 //Problem 5
-
+void numTriangle(int x){
+	int row = 1;
+	int spaces;
+	int spacesBtw;
+	int numCount;
+	while(row <= x){
+		spaces = x - row;
+		if(row==1){
+			for(int i=0;i<spaces;i++){
+				cout << " ";
+			}
+			cout << row << endl;
+		}
+		else if (row < x){
+			spacesBtw = (2*row)-3;
+			for(int i=0;i<spaces;i++){
+				cout << " ";
+			}
+			cout << row;
+			for(int i=0;i<spacesBtw;i++){
+				cout << " ";
+			}
+			cout << row << endl;
+		}
+		else{
+			numCount = (2*row)-1;
+			for(int i=0;i<numCount;i++){
+				if(i==numCount-1){
+					cout << row << endl;
+				}
+				else{
+					cout << row;
+				}
+			}
+		}
+		row++;
+	}
+}
 
